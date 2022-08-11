58a17c64d1e2c6a665a2a6c5c515b244e578f07d
yurimika <yurimikan2017@gmail.com>
2022-08-11 12:48:54 +0000

    practice 2

---
+++ app.js
@@ -0,0 +1,42 @@
+let max = 100;
+let num = 1;
+let count = 0;
+
+while(num < max){
+  num = num * 2;
+  count = count + 1;
+}
+
+alert('2をかけ続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+let orenge = 120;
+let apple = 120;
+
+if (orenge > apple){
+  alert('みかんよりリンゴが安い');
+} else if (orenge == apple){
+  alert('みかんとリンゴは同じ額');
+} else if (orenge < apple){
+  alert('リンゴよりみかんが安い');
+}
+
+
+
+
+let hello = 'Hello World';
+alert(hello);
+
+let init1 = 1;
+alert(init1);
+
+let float1 = 3.14;
+alert(float1);
+
+let yurina = '由梨奈だよー';
+alert(yurina);
+
+alert("aaaaaaaaa" + "iyaaaaaaaaaaa");
+
+alert(hello + yurina);
+
+alert(init1 / float1);

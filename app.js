fe5261e26c303165a216481bfd1d87be11d1c92a
yurimika <yurimikan2017@gmail.com>
2022-09-02 14:45:49 +0000

    commit1

--- app.js
+++ app.js
@@ -41,7 +41,8 @@ alert(hello + yurina);
 
 alert(init1 / float1);
 
-let nicname = "taro"
-console.log(nicname)
-let nickname = "ichiro"
-console.log(nickname)
+let nicname = "taro";
+console.log(nicname);
+let nickname = "ichiro";
+console.log(nickname);
+

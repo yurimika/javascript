37f1314a630f40a04440dbfc3057f6af4d9ba8e6
yurimika <yurimikan2017@gmail.com>
2022-09-08 16:45:23 +0000

    commit 4

--- app.js
+++ app.js
@@ -1,26 +1,9 @@
-let max = 100;
-let num = 1;
-let count = 0;
+let int1 = 1;
+let int2 = -10;
+let float1 = 3.14;
+let str1 = 'JavaScriptを覚えよう';
 
-while(num < max){
-  num = num * 2;
-  count = count + 1;
-}
-
-alert('2をかけ続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
-
-let orenge = 120;
-let apple = 120;
-
-if (orenge > apple){
-  alert('みかんよりリンゴが安い');
-} else if (orenge == apple){
-  alert('みかんとリンゴは同じ額');
-} else if (orenge < apple){
-  alert('リンゴよりみかんが安い');
-}
-
-var nickname = "taro";
-console.log(nickname);
-var nickname = "ichiro";
-console.log(nickname)
+alert(4 + 3);
+alert(8 - 5);
+alert(2 * 6);
+alert(10 / 2);

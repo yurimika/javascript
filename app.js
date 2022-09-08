d4bd1f5e4a08064d264a693b3bccb470aadf6b93
yurimika <yurimikan2017@gmail.com>
2022-09-08 16:47:06 +0000

    commit5

--- app.js
+++ app.js
@@ -7,3 +7,27 @@ alert(4 + 3);
 alert(8 - 5);
 alert(2 * 6);
 alert(10 / 2);
+
+alert('Hello' + 'World');
+
+let orange = 100;
+let apple = 120;
+
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+} else if(orange == apple){
+  alert('みかんとりんごが同じ値段');
+} else{
+  alert('みかんの値段がりんごより高い');
+}
+
+let max = 100;
+let num = 1;
+let count = 0;
+
+while(num < max){
+  num = num * 2;
+  count = count + 1;
+}
+
+alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');

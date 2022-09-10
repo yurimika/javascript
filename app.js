42fc042768dd6ba2c788cb01d18c9f76ca00f783
yurimika <yurimikan2017@gmail.com>
2022-09-10 16:27:36 +0000

    commit7

--- app.js
+++ app.js
@@ -76,3 +76,38 @@ console.log(nickname)
 nickname = "jiro"
 console.log(nickname)
 
+var nickname = "taro"
+console.log(nickname)
+var nickname = "ichiro"
+console.log(nickname);
+
+// letによる再宣言
+let nickname = "taro"
+console.log(nickname)
+let nickname = "ichiro"
+console.log(nickname);
+
+// constによる再宣言
+const nickname = "taro"
+console.log(nickname)
+const nickname = "ichiro"
+console.log(nickname);
+
+// varによる再代入
+var nickname = "taro"
+console.log(nickname)
+nickname = "jiro"
+console.log(nickname);
+
+// letによる再代入
+let nickname = "taro"
+console.log(nickname)
+nickname = "jiro"
+console.log(nickname);
+
+// constによる再代入
+const nickname = "taro"
+console.log(nickname)
+nickname = "jiro"
+console.log(nickname)
+

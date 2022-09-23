f41cf3f147322a56b12fa1b7ebee3fa41d03e2db
yurimika <yurimikan2017@gmail.com>
2022-09-23 17:07:37 +0000

    commit17

--- app.js
+++ app.js
@@ -110,3 +110,116 @@ alert(promptStr);
 let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
 
 alert('あなたの選んだ手は' + user_hand + 'です。');
+
+let int1 = 1;
+let int2 = -10;
+let float1 = 3.14;
+let str1 = 'JavaScriptを覚えよう';
+
+alert(4 + 3);
+alert(8 - 5);
+alert(2 * 6);
+alert(10 / 2);
+
+alert('Hello' + 'World');
+
+let orange = 100;
+let apple = 120;
+
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+} else if(orange == apple){
+  alert('みかんとりんごが同じ値段');
+} else{
+  alert('みかんの値段がりんごより高い');
+}
+
+let max = 100;
+let num = 1;
+let count = 0;
+
+while(num < max){
+  num = num * 2;
+  count = count + 1;
+}
+
+alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+let int1 = 1;
+let int2 = -10;
+let float1 = 3.14;
+let str1 = 'JavaScriptを覚えよう';
+
+alert(4 + 3);
+alert(8 - 5);
+alert(2 * 6);
+alert(10 / 2);
+
+alert('Hello' + 'World');
+
+let orange = 100;
+let apple = 120;
+
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+} else if(orange == apple){
+  alert('みかんとりんごが同じ値段');
+} else{
+  alert('みかんの値段がりんごより高い');
+}
+
+let max = 100;
+let num = 1;
+let count = 0;
+
+while(num < max){
+  num = num * 2;
+  count = count + 1;
+}
+
+alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+function addString(strA){
+  let addStr = "Hello " + strA;
+  return addStr;
+}
+
+// メイン部分
+let alertString;
+alertString = addString("WebCamp");
+
+// 作成した関数を呼び出す
+alert(alertString);
+
+// 作成した関数
+function addString(strA){
+  let addStr = "Hello " + strA;
+  return addStr;
+}
+
+let promptStr = prompt('何か好きな文字を入力してください。');
+
+alert(promptStr);
+
+let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+
+alert('あなたの選んだ手は' + user_hand + 'です。');// メイン部分
+let alertString;
+alertString = addString("WebCamp");
+
+// 作成した関数を呼び出す
+alert(alertString);
+
+// 作成した関数
+function addString(strA){
+  let addStr = "Hello " + strA;
+  return addStr;
+}
+
+let promptStr = prompt('何か好きな文字を入力してください。');
+
+alert(promptStr);
+
+let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+
+alert('あなたの選んだ手は' + user_hand + 'です。');

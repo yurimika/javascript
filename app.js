3e0efc1119da1704bcafa1a1760b81c3cef9a4ca
yurimika <yurimikan2017@gmail.com>
2022-10-16 17:30:55 +0000

    commit28

--- app.js
+++ app.js
@@ -158,3 +158,153 @@ let int2 = -10;
 let float1 = 3.14;
 let str1 = 'JavaScriptを覚えよう';
 
+alert('Hello' + 'World');
+
+let orange = 100;
+let apple = 120;
+
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+} else if(orange == apple){
+  alert('みかんとりんごが同じ値段');
+} else{
+  alert('みかんの値段がりんごより高い');
+}
+
+let max = 100;
+let num = 1;
+let count = 0;
+
+while(num < max){
+  num = num * 2;
+  count = count + 1;
+}
+
+alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+
+let int1 = 1;
+let int2 = -10;
+let float1 = 3.14;
+let str1 = 'JavaScriptを覚えよう';
+
+alert(4 + 3);
+alert(8 - 5);
+alert(2 * 6);
+alert(10 / 2);
+
+alert('Hello' + 'World');
+
+let orange = 100;
+let apple = 120;
+
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+} else if(orange == apple){
+  alert('みかんとりんごが同じ値段');
+} else{
+  alert('みかんの値段がりんごより高い');
+}
+
+let int1 = 1;
+let int2 = -10;
+let float1 = 3.14;
+let str1 = 'JavaScriptを覚えよう';
+
+alert(4 + 3);
+alert(8 - 5);
+alert(2 * 6);
+alert(10 / 2);
+
+alert('Hello' + 'World');
+
+let orange = 100;
+let apple = 120;
+
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+} else if(orange == apple){
+  alert('みかんとりんごが同じ値段');
+} else{
+  alert('みかんの値段がりんごより高い');
+}
+
+let int1 = 1;
+let int2 = -10;
+let float1 = 3.14;
+let str1 = 'JavaScriptを覚えよう';
+
+alert(4 + 3);
+alert(8 - 5);
+alert(2 * 6);
+alert(10 / 2);
+
+alert('Hello' + 'World');
+
+let orange = 100;
+let apple = 120;
+
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+} else if(orange == apple){
+  alert('みかんとりんごが同じ値段');
+} else{
+  alert('みかんの値段がりんごより高い');
+}
+
+let int1 = 1;
+let int2 = -10;
+let float1 = 3.14;
+let str1 = 'JavaScriptを覚えよう';
+
+alert(4 + 3);
+alert(8 - 5);
+alert(2 * 6);
+alert(10 / 2);
+
+alert('Hello' + 'World');
+
+let orange = 100;
+let apple = 120;
+
+
+
+
+
+
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+} else if(orange == apple){
+  alert('みかんとりんごが同じ値段');
+} else{
+  alert('みかんの値段がりんごより高い');
+}
+
+let int1 = 1;
+let int2 = -10;
+let float1 = 3.14;
+let str1 = 'JavaScriptを覚えよう';
+
+alert(4 + 3);
+alert(8 - 5);
+alert(2 * 6);
+alert(10 / 2);
+
+alert('Hello' + 'World');
+
+let orange = 100;
+let apple = 120;
+
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+} else if(orange == apple){
+  alert('みかんとりんごが同じ値段');
+} else{
+  alert('みかんの値段がりんごより高い');
+}
+
+let int1 = 1;
+let int2 = -10;
+let float1 = 3.14;
+let str1 = 'JavaScriptを覚えよう';
+
